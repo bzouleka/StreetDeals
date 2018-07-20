d298b4a93628a2b9524b9e9b78d2aa7660095237
bzouleka <bzouleka@gmail.com>
2018-07-20 11:55:21 +0200

    Register and login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterFormType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +14,15 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
-    /**
-     * @Route("/security", name="security")
-     */
-    public function index()
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
-    }
+//    /**
+//     * @Route("/security", name="security")
+//     */
+//    public function index()
+//    {
+//        return $this->render('security/index.html.twig', [
+//            'controller_name' => 'SecurityController',
+//        ]);
+//    }
 
     /**
      * @param Request $request
@@ -28,6 +31,35 @@ class SecurityController extends Controller
      * @Route("/login", name="login")
      */
 
+    /**
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration(Request $request, ObjectManager $manager){
+
+        $user = new User();
+
+        $form = $this->createForm(RegisterFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return $this->render('security/registration.html.twig', ['form' => $form->createView()]);
+    }
+
+
+    /**
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/login", name="security_login")
+     */
     public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
         // get the login error if there is one
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,14 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -35,11 +38,13 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     *
      */
     private $Email;
 
     /**
      * @ORM\Column(type="string", length=8)
+     *
      */
     private $Password;
 
@@ -147,5 +152,59 @@ class User
         }
 
         return $this;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
