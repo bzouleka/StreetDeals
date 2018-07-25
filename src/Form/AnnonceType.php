baa72419cd9cfc429e71bab88b6d058ed60402c3
bzouleka <bzouleka@gmail.com>
2018-07-25 14:46:02 +0200

    amelioration du formulaire annonce

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +15,13 @@ class AnnonceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Title')
-            ->add('Description')
+            ->add('Title', TextType::class)
+            ->add('Description', TextareaType::class)
             ->add('CreateAt')
-            ->add('Photo')
+            ->add('Photo', FileType::class)
             ->add('Region')
-            ->add('Category')
-            ->add('Other')
+
+
 
         ;
     }
